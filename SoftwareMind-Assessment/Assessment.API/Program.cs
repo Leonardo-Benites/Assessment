430b1285dfb193022ef348538df1cb19a7fa2904
11992189951 <11992189951@bellinatiperez.com.br>
2025-01-06 15:39:24 -0300

    feat: added migrations, removed startup and updated program.cs

--- SoftwareMind-Assessment/Assessment.API/Program.cs
+++ SoftwareMind-Assessment/Assessment.API/Program.cs
@@ -1,16 +1,59 @@
-using Assessment.API;
+using Assessment.Application.Interfaces;
+using Assessment.Application.Services;
+using Assessment.Infrastructure.Context;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD");
+string sqlConnection = builder.Configuration.GetConnectionString("MySqlConnection");
+
+string connectionString = sqlConnection + password + ";";
+
+// Registering services
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseMySQL(connectionString));
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+
+builder.Services.AddAutoMapper(typeof(Program));  // AutoMapper configuration
+builder.Services.AddAutoMapper(typeof(MappingProfileService));
+
+builder.Services.AddControllers();
+
+// Add Swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    app.UseDeveloperExceptionPage();
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    // Enable Swagger UI in Development
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        options.RoutePrefix = string.Empty;  // Serve Swagger at root (http://localhost)
+    });
 }
+else
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseRouting();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();  // Ensure API controllers are mapped
+
+app.Run();
---
+++ SoftwareMind-Assessment/Assessment.Infrastructure/Migrations/20250105215735_InitialCreate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Assessment.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    HireDate = table.Column<DateTime>(type: "datetime(6)", nullable: true),
+                    Phone = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    Address = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentId",
+                table: "Employee",
+                column: "DepartmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
